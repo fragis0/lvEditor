76185f09a281e305e4828e40aa985ee525c71fc4
hvdev <162542715+fragis0@users.noreply.github.com>
2025-06-16 20:23:18 -0700

    a new update!
    this update includes undo/redo function, more is soon...

--- src/lvEditor.java
+++ src/lvEditor.java
@@ -1,21 +1,35 @@
-package src;
-
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.BadLocationException;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Properties;
-import java.lang.Object;
 import com.formdev.flatlaf.FlatLightLaf;
-	 
+import java.util.List;
+
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.CompoundEdit;
+import javax.swing.undo.UndoableEdit;
+
 public class lvEditor { 
 
     private static File currentFile = null;
     private static int fontSize;
+    private static UndoManager undoManager;
 
     public static void main(String[] args) {
- 
+        
+        // sanwic beter
+        //    -stockman50
         Properties config = new Properties();
         try (FileInputStream in = new FileInputStream("config.properties")) {
             config.load(in);
@@ -24,10 +38,10 @@ public class lvEditor {
             fontSize = 15;
             saveConfig(config);
         }
+
+        undoManager = new UndoManager();
+
 	    JFrame frame = new JFrame("lvEditor");
-        /* add comment from next 2 lines after these 2 comments when running .java so it doesnt crash, add it back when making a fat jar (uber jar) using build commands in README */
-        ImageIcon icon = new ImageIcon(lvEditor.class.getResource("/icon.png"));
-        frame.setIconImage(icon.getImage());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
         frame.setSize(300, 300);
         frame.setLocationRelativeTo(null);
@@ -39,6 +53,8 @@ public class lvEditor {
         }
         
         JTextArea textwrite = new JTextArea();
+        textwrite.getDocument().addUndoableEditListener(undoManager);
+        undoManager.maxBufferSize = 10;
 
         if (args.length > 0) {
             String filename = args[0];
@@ -59,7 +75,8 @@ public class lvEditor {
 
         JMenuBar menubar = new JMenuBar();
         JMenu file = new JMenu("File");
-        JMenu format = new JMenu("Format");
+        JMenu view = new JMenu("View");
+        JMenu edit = new JMenu("Edit");
         JMenu help = new JMenu("Help");
 
         JMenuItem save = new JMenuItem("Save", null);
@@ -73,6 +90,11 @@ public class lvEditor {
         JMenuItem zoomout = new JMenuItem("Zoom out", null);
         zoomout.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, InputEvent.CTRL_DOWN_MASK));
 
+        JMenuItem undo = new JMenuItem("Undo", null);
+        undo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK));
+        JMenuItem redo = new JMenuItem("Redo", null);
+        redo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, InputEvent.CTRL_DOWN_MASK));
+
         JMenuItem report = new JMenuItem("Report issue", null);
         JMenuItem about = new JMenuItem("About", null);
 
@@ -81,8 +103,11 @@ public class lvEditor {
         file.addSeparator();
         file.add(exit);
 
-        format.add(zoomin);
-        format.add(zoomout);
+        view.add(zoomin);
+        view.add(zoomout);
+
+        edit.add(undo);
+        edit.add(redo);
 
         help.add(report);
         help.addSeparator();
@@ -95,6 +120,7 @@ public class lvEditor {
                 try {
                     String content = Files.readString(openedFile.toPath());
                     textwrite.setText(content);
+                    undoManager.reset();
                 } catch (IOException ex) {
                     JOptionPane.showConfirmDialog(frame, "Error reading file!", "lvEditor", JOptionPane.WARNING_MESSAGE);
                 }
@@ -141,11 +167,28 @@ public class lvEditor {
         });
 
         about.addActionListener(e -> {
-            JOptionPane.showMessageDialog(frame, "lvEditor 1.0\nSimple, modern look and small text editor.\n    [ developed by: hvdev ]", "lvEditor", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(frame, "lvEditor 1.1.0\nSimple, modern look and small text editor.\n    [ developed by: hvdev ]", "lvEditor", JOptionPane.INFORMATION_MESSAGE);
+        });
+
+        undo.addActionListener(e -> {
+            if (undoManager.canUndo()) {
+                undoManager.undo();
+            } else {
+                Toolkit.getDefaultToolkit().beep();
+            }
+        });
+
+        redo.addActionListener(e -> {
+            if (undoManager.canRedo()) {
+                undoManager.redo();
+            } else {
+                Toolkit.getDefaultToolkit().beep();
+            }
         });
 
         menubar.add(file);
-        menubar.add(format);
+        menubar.add(view);
+        menubar.add(edit);
         menubar.add(help);
         frame.setJMenuBar(menubar);
 
@@ -191,5 +234,157 @@ public class lvEditor {
         }
 
         return null;
+    }
+
+    public static class UndoManager extends AbstractUndoableEdit implements UndoableEditListener {
+        private String lastEditName = null;
+        private List<MergeComponentEdit> edits = new ArrayList<MergeComponentEdit>(32);
+        private MergeComponentEdit current;
+        private int pointer = -1;
+        private int maxBufferSize = 50;
+
+        private List<ChangeListener> changeListeners = new ArrayList<>(8);
+
+        public void reset() {
+            edits.clear();
+            pointer = -1;
+            lastEditName = null;
+            current = null;
+            fireStateChanged();
+        }
+
+        public void addChangeListener(ChangeListener changeListener) {
+            changeListeners.add(changeListener);
+        }
+
+        public void removeChangeListener(ChangeListener changeListener) {
+            changeListeners.remove(changeListener);
+        }
+
+        public void undoableEditHappened(UndoableEditEvent e) {
+            UndoableEdit edit = e.getEdit();
+            if (edit instanceof AbstractDocument.DefaultDocumentEvent) {
+                try {
+                    AbstractDocument.DefaultDocumentEvent event = (AbstractDocument.DefaultDocumentEvent) edit;
+                    int start = event.getOffset();
+                    int len = event.getLength();
+                    if (start + len > event.getDocument().getLength()) {
+                        createCompoundEdit();
+                        current.addEdit(edit);
+                        lastEditName = edit.getPresentationName();
+                    } else {
+
+                        String text = event.getDocument().getText(start, len);
+                        boolean isNeedStart = false;
+                        if (current == null) {
+                            isNeedStart = true;
+                        } else if (text.contains(" ")) {
+                            isNeedStart = true;
+                        } else if (lastEditName == null || !lastEditName.equals(edit.getPresentationName())) {
+                            isNeedStart = true;
+                        }
+
+                        while (pointer < edits.size() - 1) {
+                            edits.remove(edits.size() - 1);
+                            isNeedStart = true;
+                        }
+                        if (isNeedStart) {
+                            createCompoundEdit();
+                        }
+
+                        current.addEdit(edit);
+                        lastEditName = edit.getPresentationName();
+                    }
+                    fireStateChanged();
+                } catch (BadLocationException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+
+        public void createCompoundEdit() {
+            if (current == null || current.getLength() > 0) {
+                current = new MergeComponentEdit();
+                if (edits.size() >= maxBufferSize) {
+                    edits.remove(0);
+                    if (pointer > 0) {
+                        pointer--;
+                    } else {
+                        pointer = -1;
+                    }
+                }
+                edits.add(current);
+                pointer = edits.size() - 1;
+            }
+        }
+
+        public void undo() throws CannotUndoException {
+            if (!canUndo()) {
+                throw new CannotUndoException();
+            }
+
+            MergeComponentEdit u = edits.get(pointer);
+            u.undo();
+            pointer--;
+
+            fireStateChanged();
+        }
+
+        public void redo() throws CannotUndoException {
+            if (!canRedo()) {
+                throw new CannotUndoException();
+            }
+
+            pointer++;
+            MergeComponentEdit u = edits.get(pointer);
+            u.redo();
+
+            fireStateChanged();
+        }
+
+        public boolean canUndo() {
+            return pointer >= 0;
+        }
+
+        public boolean canRedo() {
+            return edits.size() > 0 && pointer < edits.size() - 1;
+        }
+
+        protected void fireStateChanged() {
+            if (changeListeners.isEmpty()) {
+                return;
+            }
+            ChangeEvent evt = new ChangeEvent(this);
+            for (ChangeListener listener : changeListeners) {
+                listener.stateChanged(evt);
+            }
+        }
+
+        protected class MergeComponentEdit extends CompoundEdit {
+            boolean isUnDone = false;
+
+            public int getLength() {
+                return edits.size();
+            }
+
+            public void undo() throws CannotUndoException {
+                super.undo();
+                isUnDone = true;
+            }
+
+            public void redo() throws CannotUndoException {
+                super.redo();
+                isUnDone = false;
+            }
+
+            public boolean canUndo() {
+                return edits.size() > 0 && !isUnDone;
+            }
+
+            public boolean canRedo() {
+                return edits.size() > 0 && isUnDone;
+            }
+
+        }
     }
 }
